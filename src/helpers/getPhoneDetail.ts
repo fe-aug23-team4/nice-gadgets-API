7e99a029b3473404fa1338dcee22a49aa756a065
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-12 01:49:22 +0200

    check path on Render.com

--- src/helpers/getPhoneDetail.ts
+++ src/helpers/getPhoneDetail.ts
@@ -5,6 +5,10 @@ import { PhoneDetail } from '../types/PhoneDetail';
 
 export async function getPhoneDetail(id: string) {
   const phonesDetailPath = path.resolve('../public/api/phones', `${id}.json`);
+
+  // eslint-disable-next-line no-console
+  console.log(path.resolve('../public/api/phones', `${id}.json`));
+
   const phonesDetailJson = await fs.readFile(phonesDetailPath, 'utf-8');
   const phonesDetail: PhoneDetail = JSON.parse(phonesDetailJson);
 
--- src/helpers/getPhones.ts
+++ src/helpers/getPhones.ts
@@ -5,6 +5,10 @@ import { Phone } from '../types/Phone';
 
 export async function getPhones() {
   const phonesPath = path.resolve('../public/api/phones.json');
+
+  // eslint-disable-next-line no-console
+  console.log(path.resolve('../public/api/phones.json'));
+
   const phonesJson = await fs.readFile(phonesPath, 'utf-8');
   const phones: Phone[] = JSON.parse(phonesJson);
 
