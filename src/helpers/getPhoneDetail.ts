99b5220a5bec9a6b3b7e6fb2d08632e5162ff39d
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-12 01:44:50 +0200

    fix path

--- src/helpers/getPhoneDetail.ts
+++ src/helpers/getPhoneDetail.ts
@@ -4,11 +4,7 @@ import path from 'path';
 import { PhoneDetail } from '../types/PhoneDetail';
 
 export async function getPhoneDetail(id: string) {
-  const phonesDetailPath = path.join(
-    __dirname,
-    '../../public/api/phones',
-    `${id}.json`,
-  );
+  const phonesDetailPath = path.resolve('../public/api/phones', `${id}.json`);
   const phonesDetailJson = await fs.readFile(phonesDetailPath, 'utf-8');
   const phonesDetail: PhoneDetail = JSON.parse(phonesDetailJson);
 
--- src/helpers/getPhones.ts
+++ src/helpers/getPhones.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { Phone } from '../types/Phone';
 
 export async function getPhones() {
-  const phonesPath = path.join(__dirname, '../../public/api', 'phones.json');
+  const phonesPath = path.resolve('../public/api/phones.json');
   const phonesJson = await fs.readFile(phonesPath, 'utf-8');
   const phones: Phone[] = JSON.parse(phonesJson);
 
