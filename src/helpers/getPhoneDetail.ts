8a89992f17022dd012507e9df8a36c6ea2640f09
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-12-12 10:54:48 +0200

    fix path on Render.com

--- src/helpers/getPhoneDetail.ts
+++ src/helpers/getPhoneDetail.ts
@@ -6,12 +6,14 @@ import { PhoneDetail } from '../types/PhoneDetail';
 export async function getPhoneDetail(id: string) {
   const phonesDetailPath = path.resolve(
     __filename,
-    '../../api/phones',
+    '../../../public/api/phones',
     `${id}.json`,
   );
 
   // eslint-disable-next-line no-console
-  console.log(path.resolve(__filename, '../../api/phones', `${id}.json`));
+  console.log(
+    path.resolve(__filename, '../../../public/api/phones', `${id}.json`),
+  );
 
   const phonesDetailJson = await fs.readFile(phonesDetailPath, 'utf-8');
   const phonesDetail: PhoneDetail = JSON.parse(phonesDetailJson);
--- src/helpers/getPhones.ts
+++ src/helpers/getPhones.ts
@@ -4,10 +4,14 @@ import path from 'path';
 import { Phone } from '../types/Phone';
 
 export async function getPhones() {
-  const phonesPath = path.resolve(__filename, '../../api', 'phones.json');
+  const phonesPath = path.resolve(
+    __filename,
+    '../../../public/api',
+    'phones.json',
+  );
 
   // eslint-disable-next-line no-console
-  console.log(path.resolve(__filename, '../../api', 'phones.json'));
+  console.log(path.resolve(__filename, '../../../public/api', 'phones.json'));
 
   const phonesJson = await fs.readFile(phonesPath, 'utf-8');
   const phones: Phone[] = JSON.parse(phonesJson);
